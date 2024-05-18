8b302fbbd83694289fa704ed8208b370f8a61a04
skywalker <baskirill.an@gmail.com>
2024-05-19 02:38:39 +0300

    feat(active-users): retrieve info about active users from backend

--- src/renderer/components/users/ActiveUsers.tsx
+++ src/renderer/components/users/ActiveUsers.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export type ActiveUsers = ActiveUser[]
+
+export interface ActiveUser {
+  fullName: string
+}
+
 
 const ActiveUsers = () => {
+  const [activeUsers, setActiveUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchActiveUsers = async () => {
+      const response = await axios.get<ActiveUsers>("http://localhost:8080/api/users/active", {
+        headers: {
+          Authorization: `Bearer ${window.localStorage.getItem('auth_token')}`,
+        },
+      });
+
+      setActiveUsers(response.data);
+    };
+
+    fetchActiveUsers()
+  }, []);
+
   return (
     <div className="active-users-wrapper">
       <h1>Active Users</h1>
       <ul>
-        <li>Kiryuxa Bas</li>
-        <li>Romich Mishura</li>
+        {activeUsers.map((user: ActiveUser) => (
+          <li key={user.fullName}>{user.fullName}</li>
+        ))}
       </ul>
     </div>
   );
