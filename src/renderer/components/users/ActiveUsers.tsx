7ddc8ba73c0f9a54caf8b15ec60a13204a9517eb
skywalker <baskirill.an@gmail.com>
2024-05-19 12:33:08 +0300

    feat(admin-panel): show user info

--- src/renderer/components/users/ActiveUsers.tsx
+++ src/renderer/components/users/ActiveUsers.tsx
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export type ActiveUsers = ActiveUser[]
 
 export interface ActiveUser {
-  fullName: string
+  fullName: string,
+  login: string,
 }
 
 
 const ActiveUsers = () => {
-  const [activeUsers, setActiveUsers] = useState([]);
+  const [activeUsers, setActiveUsers] = useState<ActiveUser[]>([]);
 
   useEffect(() => {
     const fetchActiveUsers = async () => {
-      const response = await axios.get<ActiveUsers>("http://localhost:8080/api/users/active", {
+      const response = await axios.get<ActiveUsers>('http://localhost:8080/api/users/active', {
         headers: {
           Authorization: `Bearer ${window.localStorage.getItem('auth_token')}`,
         },
@@ -22,19 +24,19 @@ const ActiveUsers = () => {
       setActiveUsers(response.data);
     };
 
-    fetchActiveUsers()
+    fetchActiveUsers();
   }, []);
 
   return (
-    <div className="active-users-wrapper">
+    <div className='active-users-wrapper'>
       <h1>Active Users</h1>
       <ul>
         {activeUsers.map((user: ActiveUser) => (
-          <li key={user.fullName}>{user.fullName}</li>
+          <li key={user.fullName}><Link to={`/user/${user.login}`}>{user.fullName}</Link></li>
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default ActiveUsers;
---
+++ src/renderer/components/users/User.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import JwtPayload from '@renderer/jwt/JwtPayload';
+import Login from '@components/login/Login';
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+import { useParams } from 'react-router';
+import Header from '@components/header/Header';
+
+export interface UserInfo {
+  fullName: string;
+  role: string;
+  username: string;
+  courses: Course[];
+}
+
+export interface Course {
+  code: string;
+  name: string;
+}
+
+
+const User = () => {
+  const { username } = useParams();
+  const [authenticated, setAuthenticated] = useState(false);
+  const [role, setRole] = useState('');
+  const [userFullName, setUserUserFullName] = useState<string>('');
+  const [userRole, setUserRole] = useState<string>('');
+  const [courses, setCourses] = useState<Course[]>([]);
+
+  useEffect(() => {
+    const jwtRaw = window.localStorage.getItem('auth_token');
+
+    if (jwtRaw === null) {
+      setAuthenticated(false);
+      return;
+    }
+
+    setAuthenticated(true);
+    const jwt = jwtDecode<JwtPayload>(jwtRaw);
+    setRole(jwt.role);
+  });
+
+  useEffect(() => {
+    console.log(username);
+    const fetchUserInfo = async () => {
+      const response = await axios.get<UserInfo>('http://localhost:8080/api/user', {
+        headers: {
+          Authorization: `Bearer ${window.localStorage.getItem('auth_token')}`,
+        },
+        params: {
+          login: username,
+        },
+      });
+
+      setUserUserFullName(response.data.fullName);
+      setUserRole(response.data.role);
+      setCourses(response.data.courses);
+    };
+
+    fetchUserInfo();
+  }, []);
+
+  if (!authenticated) {
+    return <Login />;
+  }
+
+  return (
+    <div>
+      {role === 'ROLE_ADMIN' && (
+        <div>
+          <Header role={role} />
+          <h1>{userFullName}</h1>
+          Role: {userRole}<br />
+          {courses.length > 0 && (
+            <div>
+              <p>Courses:</p>
+              <ul>
+                {courses.map((course, index) => (
+                  <li key={index}>{course.code} -- {course.name}</li>
+                ))}
+              </ul>
+            </div>)}
+        </div>)}
+    </div>
+  )
+    ;
+};
+
+export default User;
